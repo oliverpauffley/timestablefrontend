4e830813b2d0cd6f17afee062cf7180053f20635
oliverpauffley <mrpauffley@gmail.com>
2020-01-12 17:14:59 +0000

    try to fix vuetify

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from 'vuetify';
+import Vuetify from 'vuetify';
 
 Vue.config.productionTip = false
-
+Vue.use(Vuetify)
 new Vue({
   router,
   store,
-  vuetify,
+  Vuetify,
   render: function (h) { return h(App) }
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,6 @@
 </template>
 
 <script>
-// @ is an alias to /src
 
 export default {
   name: 'home',
