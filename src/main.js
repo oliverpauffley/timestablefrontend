03037b33e4e16e7a0208e3306a8d94d2dc3cc850
oliverpauffley <mrpauffley@gmail.com>
2020-01-13 08:57:17 +0000

    fix: Actually fix vuetify

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vuetify from 'vuetify';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
-Vue.use(Vuetify)
 new Vue({
   router,
   store,
-  Vuetify,
+  vuetify,
   render: function (h) { return h(App) }
 }).$mount('#app')
